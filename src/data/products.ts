736d02e9bdcc698f6a35d22c0a9a863d77743feb
fidellr <fidelramadhan@gmail.com>
2026-02-26 18:50:35 +0700

    feat: add mock data using next api route

---
+++ src/data/products.ts
@@ -0,0 +1,105 @@
+import { Product } from "@/types";
+
+export const products: Product[] = [
+  {
+    id: "d1",
+    name: "Standard Desk",
+    category: "desks",
+    price: 30,
+    iconName: "Table",
+    imageUrl:
+      "https://grovemade.imgix.net/https%3A%2F%2Fgrovemade.com%2Fshop-static%2Fshop%2Fvariant%2Fgrovemade-standing-desk-walnut-solid-program-galA-A1.jpg%3F_v%3D1712338294.99413?auto=format&ixlib=python-1.1.2&w=3000&s=feedf42cbff5782db8424747025b713a",
+  },
+  {
+    id: "d2",
+    name: "Standing Desk",
+    category: "desks",
+    price: 45,
+    iconName: "Table",
+    imageUrl:
+      "https://cdn.prod.website-files.com/62ec28c28759bdba5015b899/6863ecb375f9b740888dd0d6_desk_titel_new_3db151d44c.jpeg",
+  },
+
+  {
+    id: "c1",
+    name: "Office Chair",
+    category: "chairs",
+    price: 25,
+    iconName: "Armchair",
+    imageUrl:
+      "https://cdn.shopify.com/s/files/1/2135/0257/products/OC2757-SN.jpg?v=1611832857",
+  },
+  {
+    id: "c2",
+    name: "Ergo Chair",
+    category: "chairs",
+    price: 40,
+    iconName: "Armchair",
+    imageUrl:
+      "https://cdn.prod.website-files.com/62ec28c28759bdba5015b899/6863eca419dead1343bef567_fantech_oca259s_chair_6_b632a0c529.jpeg",
+  },
+
+  {
+    id: "ah1",
+    name: "Monitor",
+    category: "accessories",
+    price: 40,
+    iconName: "Horizontal Monitor",
+    imageUrl:
+      "https://cdn.prod.website-files.com/62ec28c28759bdba5015b899/6863ea6c097ce9fb569b8c0c_24_Full_HD_Office_Monitor_A24i_1_7f987306af.jpeg",
+  },
+   {
+    id: "av1",
+    name: "Monitor",
+    category: "accessories",
+    price: 40,
+    iconName: "Vertical Monitor",
+    imageUrl:
+      "https://media.istockphoto.com/id/153986203/photo/computer-monitor-with-clipping-path.jpg?s=612x612&w=0&k=20&c=zUzocoiP31nfnjUKTmra_3h-avdVdzJuZNh8-kC5UTI=",
+  },
+  {
+    id: "a2",
+    name: "Plant",
+    category: "accessories",
+    price: 15,
+    iconName: "Flower2",
+    imageUrl:
+      "https://greenkin.in/cdn/shop/files/zz-green-plant-eco-pot-small-red-greenkin.webp?v=1734161313&width=533",
+  },
+
+  {
+    id: "e1",
+    name: "Coffee Machine",
+    category: "coffee",
+    price: 30,
+    iconName: "Coffee",
+  },
+  {
+    id: "e2",
+    name: "Surfboard",
+    category: "outdoor",
+    price: 20,
+    iconName: "Waves",
+  },
+  {
+    id: "e3",
+    name: "Motorcycle",
+    category: "outdoor",
+    price: 150,
+    iconName: "Bike",
+  },
+  {
+    id: "e4",
+    name: "Bean Bag",
+    category: "relax",
+    price: 10,
+    iconName: "Sofa",
+  },
+  {
+    id: "e5",
+    name: "Tool Shelf",
+    category: "garage",
+    price: 25,
+    iconName: "Wrench",
+  },
+];
